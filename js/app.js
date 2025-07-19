077b0106001242f0986d3f44c5769d159f3148d1
V32V3T <ayj182003@gmail.com>
2025-07-19 14:54:12 +0530

    gi

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Script.js
 // Configuration
 const MATCH_QR_STRING = "f29cZb7Q6DuaMjYkTLV3nxR9KEqV2XoBslrHcwA8d1tZ5UeqgiWTvjNpLEsQ";
-const API_BASE_URL = "https://script.google.com/macros/s/AKfycbzy7XoHlKSka7RPzNR5RqRL3zyXR3pkkDhRcP6qy-Q5kjFnBNPByCKXu3T1OFzAiuwn0w/exec";
+const API_BASE_URL = "/api/log-attendance";
 
 // State variables
 let qrScanner = null;
